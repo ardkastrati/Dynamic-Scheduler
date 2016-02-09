0825eecedd9de9ed176b27c2967608daa82c0fef
uodov <uodov@student.kit.edu>
2016-02-09 13:30:33 +0100

    added structurefor visualiser

---
+++ code/src/visualiser/Visualiser/src/main/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package main;
+
+import visualiser.Visualiser;
+
+/**
+ *This Main class is for test purposes only 
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		JavaFXWindow.main(args);
+//		// TODO Automatisch generierter Methodenstub
+//		Visualiser v = new Visualiser();
+//		System.out.println("Debug");
+		Visualiser.mai(args);
+		DiagramScene.mai(args);
+	}
+
+}
