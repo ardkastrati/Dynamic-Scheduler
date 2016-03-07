f649b7c62a59504fd4295110de03764aad88227a
uodov <uodov@student.kit.edu>
2016-03-07 16:53:04 +0100

    Added GridVisualiser

---
+++ GUI/src/main/java/model/visualiser/GridVisualiser.java
@@ -0,0 +1,24 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+package model.visualiser;
+
+import java.util.HashMap;
+import java.util.List;
+import javafx.scene.layout.Pane;
+import model.visualiser.dataholding.Event;
+import model.visualiser.dataholding.Task;
+
+/**
+ *
+ * 
+ */
+public class GridVisualiser implements Visualiser{
+
+    @Override
+    public void getVisualisation(Pane parent, HashMap<Integer, Task> taskMap, List<Event> eventList) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
