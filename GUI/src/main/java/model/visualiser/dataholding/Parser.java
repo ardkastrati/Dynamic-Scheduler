b872ac3bb609ce2323be61095d634c864a7293d8
uodov <uodov@student.kit.edu>
2016-03-07 16:59:15 +0100

    Multiple Bugfixes

--- GUI/src/main/java/model/visualiser/dataholding/Parser.java
+++ GUI/src/main/java/model/visualiser/dataholding/Parser.java
@@ -1,72 +1,109 @@
 package model.visualiser.dataholding;
 
+import controller.ParserException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Parser {
 	
-	private HashMap<Integer,Task> taskMap;
+    private HashMap<Integer,Task> taskMap;
 	
 	//####DATA_START####
 	// line: (ID);Time;Event;Mode;Parent;Start_Time;End_Time;Rank;Param_Size;#
 	// statistic:
 	// line: ID;Runtime:Parameters[];#
 	
-	HashMap<Integer,Task> parseToTaskList() {
-		
-		
-		return null;
-		
-	}
+    HashMap<Integer,Task> parseToTaskMap() {
+        return null;	
+    }
+    
+    
 	
-	void parseBookkeeping() {
-		
-	}
+    public List<Event> parseStatistic(String path) throws FileNotFoundException, ParserException{
+        File file = new File(path);
+        Scanner input = null;
+        try {
+            input = new Scanner(file);
+        } catch (FileNotFoundException e) {
+                //TODO handle Error
+        }
+        String line = null;
+        String[] splitLine;
+            
+        long runtime = -1;
+        while(input.hasNextLine()) {
+            while(!"####DATA_START####".equals(line)) {
+                line = input.nextLine();
+            }
+            line = input.nextLine();
+            splitLine = line.split(";");
+            runtime = Long.parseLong(splitLine[0]);
+            double parameters[] = new double[splitLine.length - 1];
+            for(int i = 1; i < splitLine.length; i++) {
+                parameters[i-1] = Double.parseDouble(splitLine[1]);
+            }
+              
+        }
+        input.close();
+        return null;
+    }
 	
-	void parseStatistic(String path) {
-		File file = new File(path);
-		Scanner input = null;
-		try {
-			input = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			// TODO Automatisch generierter Erfassungsblock
-			e.printStackTrace();
-		}
-		String line = null;
-		String[] splitLine;
-		while(input.hasNextLine())  {
-			while(line != "####DATA_START####") {
-				line = input.nextLine();
-			}
-			line = input.nextLine();
-			splitLine = line.split(";");
-			int id = Integer.parseInt(splitLine[0]);
-			int time = Integer.parseInt(splitLine[1]);
-			String event = splitLine[2];
-			int rank = Integer.parseInt(splitLine[7]);
-			Task task;
-			if(taskMap.containsKey(id)) {
-				task = taskMap.get(id);
-			} else {
-				task = new Task();
-			}
-			task.setId(id);
-			switch (event) {
-			case "appered":
-				task.setAppered(time);
-				break;
-			case "started":
-				task.setStarted(time);
-				break;
-			case "ended":
-				task.setEnded(time);
-				break;
-			}
-			task.setRank(rank);
-		}
-		
-		input.close();
-	}
+    public HashMap<Integer,Task> parseBookkeeping(String path) throws FileNotFoundException, ParserException{
+	if(taskMap == null) {
+            taskMap = new HashMap<Integer, Task>();
+        }
+                
+        File file = new File(path);
+	Scanner input = new Scanner(file);
+	String line = input.nextLine();
+	String[] splitLine;
+        System.out.println("Debug");
+        while(!line.contains("####DATA_START####") && input.hasNextLine()) {
+            System.out.println(line);
+            if (!input.hasNextLine()) {
+                throw new ParserException();
+            }
+            line = input.nextLine();
+        }     
+	while(input.hasNextLine())  {
+            //System.out.println("Debug in line");
+            //System.out.println("Line:" + line);
+            line = input.nextLine();
+            splitLine = line.split(";");
+            int id = Integer.parseInt(splitLine[0]);
+            if(id == 0){
+                continue;
+            }
+            Task oldTask;
+            Task task = new Task();
+            task = taskMap.getOrDefault(id, task);        
+            long time = Long.parseLong(splitLine[1]);
+            String event = splitLine[2];
+            int rank = Integer.parseInt(splitLine[7]);
+            task.setId(id);
+            switch (event) {
+            case "Appered":
+                task.setAppered(time);
+                break;
+            case "Started":
+                task.setStarted(time);
+                break;
+            case "Ended":
+                task.setEnded(time);
+                break;
+            }
+            task.setRank(rank);
+            if(taskMap.containsKey(id)) {
+                taskMap.replace(id, task);
+            } else {
+                taskMap.put(id, task);
+            }
+        }
+        input.close();
+        System.out.println(taskMap);
+        return taskMap;
+    }
 }
