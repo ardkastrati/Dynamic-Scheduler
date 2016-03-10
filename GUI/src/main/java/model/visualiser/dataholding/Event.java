346160b6750daf4176c23a4cec1ad58916dd2bf5
uodov <uodov@student.kit.edu>
2016-03-10 20:12:47 +0100

    Added javadoc comments

--- GUI/src/main/java/model/visualiser/dataholding/Event.java
+++ GUI/src/main/java/model/visualiser/dataholding/Event.java
@@ -1,24 +1,44 @@
 package model.visualiser.dataholding;
 
+/**
+ *
+ * Represents a statistic event
+ */
 public class Event {
 	
 	private Long time;
 	
 	private Double[] parameters;
 
-	public Long getTime() {
+    /**
+     * Returns the time of this event
+     * @return the time of this event
+     */
+    public Long getTime() {
 		return time;
 	}
 
-	public void setTime(Long time) {
+    /**
+     * Sets the time of this event
+     * @param time the time of this event
+     */
+    public void setTime(Long time) {
 		this.time = time;
 	}
 
-        public Double[] getParameters() {
+    /**
+     * Returns the parameters of this event 
+     * @return the parameters of this event 
+     */
+    public Double[] getParameters() {
             return parameters;
         }
         
-        public void setParameters(Double[] parameters) {
+    /**
+     * Sets the parameters of this event 
+     * @param parameters the parameters of this event 
+     */
+    public void setParameters(Double[] parameters) {
             this.parameters = parameters;
         }
 }
