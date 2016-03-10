2c4653bf12f561ed9fcf927571298e309e9c40b3
uodov <uodov@student.kit.edu>
2016-03-10 19:56:52 +0100

    Completed parse statistic

--- GUI/src/main/java/model/visualiser/dataholding/Parser.java
+++ GUI/src/main/java/model/visualiser/dataholding/Parser.java
@@ -3,10 +3,15 @@ package model.visualiser.dataholding;
 import controller.ParserException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * This class parses statistic and bookkeeping file
+ * 
+ */
 public class Parser {
 	
     private HashMap<Integer,Task> taskMap;
@@ -20,9 +25,16 @@ public class Parser {
         return null;	
     }
     
-    
-	
+    /**
+     * Parses the statistic file. For each statistic event a Event instance is created.
+     * The Events are returned in a list
+     * @param path Path to the statistic file on the disk
+     * @return a list of statistic events
+     * @throws FileNotFoundException thrown if file was not found
+     * @throws ParserException thrown if the file was not structured as expected
+     */
     public List<Event> parseStatistic(String path) throws FileNotFoundException, ParserException{
+        List<Event> eventList = new ArrayList<Event>();
         File file = new File(path);
         Scanner input = null;
         try {
@@ -34,23 +46,37 @@ public class Parser {
         String[] splitLine;
             
         long runtime = -1;
-        while(input.hasNextLine()) {
-            while(!"####DATA_START####".equals(line)) {
-                line = input.nextLine();
+        while(!"####DATA_START####".equals(line)) {
+            if (!input.hasNextLine()) {
+                throw new ParserException();
             }
+            line = input.nextLine();
+            }
+        while(input.hasNextLine()) {
             line = input.nextLine();
             splitLine = line.split(";");
             runtime = Long.parseLong(splitLine[0]);
-            double parameters[] = new double[splitLine.length - 1];
+            Double parameters[] = new Double[splitLine.length - 1];
             for(int i = 1; i < splitLine.length; i++) {
                 parameters[i-1] = Double.parseDouble(splitLine[1]);
             }
-              
+            Event event = new Event();
+            event.setTime(runtime);
+            event.setParameters(parameters);
+            eventList.add(event);
         }
         input.close();
-        return null;
+        return eventList;
     }
 	
+    /**
+     * Parses the bookkeeping file. Each Task is stored in a instance of task.
+     * The tasks will be returned in a HashMap, indexed by the id.
+     * @param path Path to the bookkeeping file on the disk
+     * @return HashMap of tasks
+     * @throws FileNotFoundException thrown if file was not found
+     * @throws ParserException thrown if the file was not structured as expected
+     */
     public HashMap<Integer,Task> parseBookkeeping(String path) throws FileNotFoundException, ParserException{
 	if(taskMap == null) {
             taskMap = new HashMap<Integer, Task>();
@@ -82,11 +108,13 @@ public class Parser {
             task = taskMap.getOrDefault(id, task);        
             long time = Long.parseLong(splitLine[1]);
             String event = splitLine[2];
+            int parent = Integer.parseInt(splitLine[4]);
+            task.setParent(parent);
             int rank = Integer.parseInt(splitLine[7]);
             task.setId(id);
             switch (event) {
             case "Appered":
-                task.setAppered(time);
+                task.setAppaered(time);
                 break;
             case "Started":
                 task.setStarted(time);
