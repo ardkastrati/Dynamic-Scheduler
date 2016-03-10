c2d46142093f8928acc3864f89984ade159c2bac
uodov <uodov@student.kit.edu>
2016-03-10 20:20:48 +0100

    Added javadoc comments

--- GUI/src/main/java/model/visualiser/Visualiser.java
+++ GUI/src/main/java/model/visualiser/Visualiser.java
@@ -8,8 +8,19 @@ import javafx.scene.layout.Pane;
 import model.visualiser.dataholding.Event;
 import model.visualiser.dataholding.Task;
 
+/**
+ *
+ * This Interface must be implemented by every Visualiser.
+ * It provides a method to draw a diagram on a given parent pane.
+ */
 public interface Visualiser {
 	
-	public void getVisualisation(Pane parent,HashMap<Integer,Task> taskMap, List<Event> eventList);
+    /**
+     * This method generates the diagram and draws it onto the parent pane 
+     * @param parent The pane to draw the diagram on
+     * @param taskMap A hashMap of tasks
+     * @param eventList a list of events
+     */
+    public void getVisualisation(Pane parent,HashMap<Integer,Task> taskMap, List<Event> eventList);
 
 }
