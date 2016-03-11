69a60f7b2d32f79299cc91943f00ccaafab05f00
David <uldzx@student.kit.edu>
2016-03-11 03:00:47 +0100

    Little changes.

--- code/src/datamining/grid/GridDataMining.cpp
+++ code/src/datamining/grid/GridDataMining.cpp
@@ -87,7 +87,7 @@ GridDataMining::GridDataMining(int rank, int target_rank, DatabaseHandler* datab
   nr_of_tasks = 0;
   average_deviation = 0;
   //prediction_time_range
-  max_differential_time = 100; //1 second
+  max_deviation_time = 100; //1 second
   //
   max_parameter = new double[nr_of_dimensions];
   min_parameter = new double[nr_of_dimensions];
