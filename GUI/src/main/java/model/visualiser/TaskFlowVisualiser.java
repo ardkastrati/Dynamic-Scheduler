d7d3ca5f724ba5c1df29aaed0ab43a408fac13b2
uodov <uodov@student.kit.edu>
2016-04-05 19:05:35 +0200

    displayed parameters are now choosable

--- GUI/src/main/java/model/visualiser/TaskFlowVisualiser.java
+++ GUI/src/main/java/model/visualiser/TaskFlowVisualiser.java
@@ -63,7 +63,7 @@ import org.controlsfx.control.Notifications;
 
 
 public class TaskFlowVisualiser implements Visualiser {
-
+/*
     @Override
     public void getVisualisation(Pane parent, Datakeeper datakeeper) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -73,7 +73,7 @@ public class TaskFlowVisualiser implements Visualiser {
     public void run() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-        /*
+       */ 
 	@Override
 	public void getVisualisation(Pane parent, Datakeeper datakeeper) {
             
@@ -227,5 +227,5 @@ public class TaskFlowVisualiser implements Visualiser {
     public String toString() {
         return "TaskFlow";
     }
-           */
+           
 }
