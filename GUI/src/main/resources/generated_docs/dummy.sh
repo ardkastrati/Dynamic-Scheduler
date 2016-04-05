7dba0869d71fd04ce2848c81f8445120ff8a862b
Ard Kastrati <ard_kitara@hotmail.com>
2016-04-05 18:38:39 +0200

    Changed the structure of the tabs in VizualisationScene

--- GUI/src/main/resources/generated_docs/dummy.sh
+++ GUI/src/main/resources/generated_docs/dummy.sh
@@ -1,6 +1,5 @@
         #### MOAB commands
 
-#MSUB  -q singlenode
 #MSUB  -l nodes=1:ppn=4
 #MSUB  -l walltime=600
 #MSUB  -l pmem=200
@@ -16,4 +15,4 @@ cd ./test
 
         #### MPI commands
 
-/opt/bwhpc/common/compiler/intel/compxe.2015.3.187/impi/5.0.3.048/intel64/bin/mpirun ./scheduler
+/opt/bwhpc/common/compiler/intel/compxe.2015.3.187/impi/5.0.3.048/intel64/bin/mpirun ./scheduler --design master-worker --strategy fifo
